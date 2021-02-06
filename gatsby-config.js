5d893ba1cc6ae483a44271be1112e9c362460d5c
Justin Ko <justin.ko43@gmail.com>
2021-02-06 17:18:05 -0500

    add some more dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
 module.exports = {
-    plugins: [
-
-    ]
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: 'jsx', // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+  ],
+};
